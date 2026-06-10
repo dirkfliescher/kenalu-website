7d444fc04f989b5a62b6fbb596c6ed94ddeb6a05
Dirk Fliescher <dirkfliescher@MacBook-Pro-3.local>
2026-06-10 15:49:34 +0200

    add storyblok lib

--- app/lib/storyblok.js
+++ app/lib/storyblok.js
@@ -2,10 +2,7 @@ import StoryblokClient from 'storyblok-js-client';
 
 const Storyblok = new StoryblokClient({
   accessToken: process.env.STORYBLOK_TOKEN,
-  cache: {
-    clear: 'auto',
-    type: 'memory',
-  },
+  cache: { clear: 'auto', type: 'memory' },
 });
 
 export default Storyblok;
