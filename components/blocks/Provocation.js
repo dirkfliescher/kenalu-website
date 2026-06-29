c99ad1e4bc5b266f8f0a465ed99cf48a1d6d08d9
Dirk Fliescher <dirk@fliescher.ch>
2026-06-29 17:58:26 +0200

    design: Provocation 2x2 Grid, breiterer Container, Headline oben

--- components/blocks/Provocation.js
+++ components/blocks/Provocation.js
@@ -4,8 +4,8 @@ export default function Provocation({ blok }) {
   return (
     <section className="provocation">
       <span className="provocation-mark" aria-hidden="true">&#8220;</span>
-      <div className="container container--narrow provocation-grid">
-        <div>
+      <div className="container provocation-grid">
+        <div className="provocation-intro">
           {blok.provocation_label && <div className="section-label">{blok.provocation_label}</div>}
           {blok.provocation_headline && <p className="lead">{blok.provocation_headline}</p>}
           {blok.provocation_text && <p className="body-text">{blok.provocation_text}</p>}
